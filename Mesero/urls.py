8bff940cf3cd9b97daffdc6b68328b10186bb961
José Alvarez Mora <71517461+jose124-max@users.noreply.github.com>
2024-02-20 03:34:46 -0500

    Update urls.py

--- Mesero/urls.py
+++ Mesero/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('mesero/<int:id_cuenta>/mesa/<int:id_mesa>/pedidos/', pedidos_del_mesero, name='pedidos_mesero_mesa'),
     path('pedidos/', ListaPedidos.as_view(), name='pedidos'),
     path('pedidoslocal/', TomarPedidoSinMesa.as_view(), name='tomar_pedido_2'),
+    path('listpedidos/', ListaPedidosMesero.as_view(), name='ListaPedidosMesero'),
+    path('confirmarpedido/', ConfirmarPedido.as_view(), name='ConfirmarPedido'),
 ]
