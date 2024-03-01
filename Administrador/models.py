4e5e4d67b75d41ef4bddd6091aa55cea6e4ff819
bryan robalino <eduardochuez_2003@outlook.com>
2024-03-01 17:21:31 -0500

    agregando modelo para datos bancarios

--- Administrador/models.py
+++ Administrador/models.py
@@ -14,3 +14,16 @@ class Administrador(models.Model):
     class Meta:
         managed = False
         db_table = 'administrador'
+
+class datosBancarios(models.Model):
+    id_cuenta = models.AutoField(primary_key=True)
+    nombre_banco=models.CharField(max_length=100)
+    tipo_cuenta=models.CharField(max_length=50)
+    num_cuenta=models.CharField(max_length=100)
+    nombreapellidos=models.CharField(max_length=100)
+    identificacion=models.CharField(max_length=100)
+    correoelectronico=models.CharField(max_length=256)
+
+    class Meta:
+        managed=False
+        db_table='datos_bancarios'
